c0f92ace07f0a09330a7bca805ba2a99eadbed6f
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-12 10:54:07 -0700

    Create run_analysis.R stub

---
+++ GetData/run_analysis.R
@@ -0,0 +1,5 @@
+# not so much an analysis, as *tidying up* of the UCI HAR dataset
+
+clean_that_data <- function(dir = "./UCI HAR Dataset") {
+    ## will, hopefully, someday, spit out a tidy table of the data
+}
