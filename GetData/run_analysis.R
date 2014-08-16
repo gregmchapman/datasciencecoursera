fe7b05fa8922be6b1197919ce34bfa082cfa5141
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-16 16:21:55 -0700

    run_analysis.R practically finished.

--- GetData/run_analysis.R
+++ GetData/run_analysis.R
@@ -18,22 +18,42 @@ clean_that_data <- function(dir = "./UCI HAR Dataset") {
     
     message("This could take a while, why don't you grab some coffee?")
     
+    require(reshape2) # we want to melt() and dcast()
+    
+    # I'm not going to assume you have the data yet
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    message("Downloading data...")
+    download.file(url=fileUrl, destfile="Dataset.zip")
+    message("Extracting data...")
+    unzip("Dataset.zip", exdir=".")
+    message("Ready to tidy.\n")
+    
+    
     # first, let's read in all the relevant files
-    testSubjects  <- read.table(paste0(dir, "/test/subject_test.txt"))
-    trainSubjects <- read.table(paste0(dir, "/train/subject_train.txt"))
-    testActivities  <- read.table(paste0(dir, "/test/y_test.txt"))
-    trainActivities <- read.table(paste0(dir, "/train/y_train.txt"))
-    testData  <- read.table(paste0(dir, "/test/X_test.txt"))
-    trainData <- read.table(paste0(dir, "/train/X_train.txt"))
-    features <- read.table(paste0(dir, "/features.txt"))
+    message("Loading data...")
+    dir = "./UCI HAR Dataset"
+    testSubjects  <- read.table(paste0(dir, "/test/subject_test.txt"), 
+                                colClasses="factor", nrows=2950)
+    trainSubjects <- read.table(paste0(dir, "/train/subject_train.txt"),
+                                colClasses="factor", nrows=7400)
+    testActivities  <- read.table(paste0(dir, "/test/y_test.txt"),
+                                  colClasses="factor", nrows=2950)
+    trainActivities <- read.table(paste0(dir, "/train/y_train.txt"),
+                                  colClasses="factor", nrows=7400)
+    testData  <- read.table(paste0(dir, "/test/X_test.txt"),
+                            colClasses="numeric", nrows=2950)
+    trainData <- read.table(paste0(dir, "/train/X_train.txt"),
+                            colClasses="numeric", nrows=7400)
+    features <- read.table(paste0(dir, "/features.txt"), nrows=600)
     activityNames <- read.table(paste0(dir, "/activity_labels.txt"))
     
     # phew, that's done, now let's mess with the feature names a bit
+    message("Massaging data... (How's that coffee?)")
     featureNames <- features[, 2]
-    featureNames <- gsub("()", "", featureNames, fixed=T) # get rid of empty parens
-    # featureNames <- gsub("[(),-]", "\\.", featureNames)     # replace other problem chars with dots
-                                                          # this one doesn't seem to be working...
-                                                          # but it also doesn't seem to matter...
+    featureNames <- gsub("()", "_", featureNames, fixed=T) # get rid of empty parens
+                                                           # the replacement with underscores
+                                                           # is a kludge to get grepl() to work below
+    featureNames <- gsub("[(),-]", "", featureNames)       # remove other problem chars
     
     # next we'll duct-tape the whole mess together
     subjects <- rbind(testSubjects, trainSubjects)
@@ -45,12 +65,7 @@ clean_that_data <- function(dir = "./UCI HAR Dataset") {
     colnames(enchilada) <- c("subject", "activity", featureNames)
     
     # let's get those activities named
-    enchilada$activity <- factor(enchilada$activity)
     levels(enchilada$activity) <- activityNames$V2
-    
-    # now let's order by subject/activity (can you tell I'm putting off subsetting?)
-    enchilada$subject <- factor(enchilada$subject)
-    enchilada <- enchilada[order(c(enchilada$subject, enchilada$activity)),]
     
     # alright, let's get the subsetting over with
     # I'm taking a fairly narrow view of "Extract only the measurements on the mean and standard deviation"
@@ -58,11 +73,24 @@ clean_that_data <- function(dir = "./UCI HAR Dataset") {
     #   so that means I'm ignoring 'meanFreq' and all of the angle() sub-variables
     enchilada <- cbind(subject=enchilada$subject,
                        activity=enchilada$activity,
-                       enchilada[, grepl("(mean|std)[^F]", colnames(enchilada), perl=T)])
+                       enchilada[, grepl("(mean|std)_", colnames(enchilada))])
     
-    ### If we've gotten this far w/o errors, let's output the reduced dataset to play with interactively
-    enchilada
     
-    ### Issues: I have 10000 NA's that I should probably deal with?
-    ### That damned grepl() (As it stands, does *not* include end-of-string mean|std)
+    message("Reshaping data...")
+    # first make sure subjects are ordered as expected
+    enchilada$subject <- ordered(enchilada$subject, c("1", "2", "3", "4", "5",
+                                 "6", "7", "8", "9", "10", "11", "12", "13",
+                                 "14", "15", "16", "17", "18", "19", "20", 
+                                 "21", "22", "23", "24", "25", "26", "27",
+                                 "28", "29", "30")) # there's prob. a better way to have done that...
+    
+    enchiladaMelt <- melt(enchilada, id = c("subject", "activity"), 
+                                     measure.vars = colnames(enchilada)[3:68])
+    meanCast <- dcast(enchiladaMelt, subject + activity ~ variable, mean)
+    meanCast <- meanCast[order(meanCast$subject), ]
+    
+    message("Writing data...")
+    write.table(meanCast, file="tidied_UCI_HAR_data.txt", row.names=F, quote=F)
+    message("Finished!")
+    meanCast
 }
