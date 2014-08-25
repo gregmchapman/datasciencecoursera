920612ceec454dc00a85e944e6f0aba8678b6a54
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-24 17:51:32 -0700

    Stub out best.R, tests for best.R

---
+++ RProg/Assignment 3/test_best.R
@@ -0,0 +1,8 @@
+test_that("best.R matches sample output", {
+    expect_that(best("TX", "heart attack"), equals("CYPRESS FAIRBANKS MEDICAL CENTER"))
+    expect_that(best("TX", "heart failure"), equals("FORT DUNCAN MEDICAL CENTER"))
+    expect_that(best("MD", "heart attack"), equals("JOHNS HOPKINS HOSPITAL, THE"))
+    expect_that(best("MD", "pneumonia"), equals("GREATER BALTIMORE MEDICAL CENTER"))
+    expect_that(best(("BB", "heart attack"), throws_error("invalid state"))
+    expect_that(best(("NY", "hert attack"), throws_error("invalid outcome"))
+})
