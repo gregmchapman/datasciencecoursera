c17fc5355df1f9ea10766937b9d3ac16e92ba20c
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-25 19:03:45 -0700

    Stubbed out rankhospital.R, rankall.R, tests (rankall_test.R is TODO)

---
+++ RProg/Assignment 3/test_rankall.R
@@ -0,0 +1,5 @@
+test_that("rankall.R matches sample output", {
+    # actually not sure how to test the output
+    # as the samples use head() and tail
+    # I'll have to come back to this one.
+})
---
+++ RProg/Assignment 3/test_rankhospital.R
@@ -0,0 +1,5 @@
+test_that("rankhospital.R matches sample output", {
+    expect_that(rankhospital("TX", "heart failure", 4), equals("DETAR HOSPITAL NAVARRO"))
+    expect_that(rankhospital("MD", "heart attack", "worst"), equals("HARFORD MEMORIAL HOSPITAL"))
+    expect_that(rankhospital("MN", "heart attack", 5000), equals(NA))
+})
