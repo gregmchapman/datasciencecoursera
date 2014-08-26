a9495bd62349d1cab749b06ab92513d991893d52
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-26 16:18:10 -0700

    best.R passes 2 tests

--- RProg/Assignment 3/best.R
+++ RProg/Assignment 3/best.R
@@ -1,7 +1,18 @@
 best <- function(state, outcome) {
     ## Read outcome data
-    
+    outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+    # trim out most of the gristle (the comparisons and bounds could go to, but I'm curious about them)
+    outcomeData <- outcomeData[, c(2, 7, 11:14, 17:20, 23:26)]
+    colnames(outcomeData) <- c("hospitalName", "state", "heartAttack30DayMortality", 
+                               "heartAttackComparison", "heartAttackLower", "heartAttackUpper", 
+                               "heartFailure30DayMortality", "heartFailureComparison", 
+                               "heartFailureLower", "heartFailureUpper", "pneumonia30DayMortality", 
+                               "pneumoniaComparison", 'pneumoniaLower', "pneumoniaUpper")
     ## Check that state and outcome are valid
+    validOutcome <- outcome %in% c("heart attack", "heart failure", "pneumonia")
+    validState   <- state %in% outcomeData$state
+    if (!validOutcome) { stop("invalid outcome") }
+    if (!validState)   { stop("invalid state") }
     
     ## Return hospital name in that state with lowest 30-day death rate
 }
--- RProg/Assignment 3/test_best.R
+++ RProg/Assignment 3/test_best.R
@@ -3,6 +3,6 @@ test_that("best.R matches sample output", {
     expect_that(best("TX", "heart failure"), equals("FORT DUNCAN MEDICAL CENTER"))
     expect_that(best("MD", "heart attack"), equals("JOHNS HOPKINS HOSPITAL, THE"))
     expect_that(best("MD", "pneumonia"), equals("GREATER BALTIMORE MEDICAL CENTER"))
-    expect_that(best(("BB", "heart attack"), throws_error("invalid state"))
-    expect_that(best(("NY", "hert attack"), throws_error("invalid outcome"))
+    expect_that(best("BB", "heart attack"), throws_error("invalid state"))
+    expect_that(best("NY", "hert attack"), throws_error("invalid outcome"))
 })
