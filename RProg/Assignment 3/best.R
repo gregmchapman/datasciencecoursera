275f0934c057c3cabc76013c1eda50274c28d205
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-27 15:39:32 -0700

    rankhospital.R passes all tests and submits successfully
    minor tweaks to best.R, comments etc.

--- RProg/Assignment 3/best.R
+++ RProg/Assignment 3/best.R
@@ -8,7 +8,7 @@ best <- function(state, outcome) {
 
     require(dplyr) # because _fuck_ it makes things easier
     
-    ## Read outcome data
+    ## Read (and structure) outcome data
     columnClasses <- replicate(46, NULL)
     columnClasses[c(2, 7, 11, 17, 23)] <- "character"
     outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses=columnClasses,
@@ -25,6 +25,7 @@ best <- function(state, outcome) {
     if (!validOutcome) { stop("invalid outcome") }
     if (!validState)   { stop("invalid state") }
     
+    ## Actually solve the problem
     by_state <- group_by(outcomeData, stateID)
     minSet <- switch(outcome, 
                      "heart attack" = filter(by_state, heartAttack == min(heartAttack, na.rm=T)),
@@ -32,8 +33,6 @@ best <- function(state, outcome) {
                      "pneumonia" = filter(by_state, pneumonia == min(pneumonia, na.rm=T)))
     winners <- minSet[minSet$stateID == state, "hospitalName"]
     
-    
     ## Return hospital name in that state with lowest 30-day death rate
-    #if (length(winners) == 1) { return(winners) }
     sort(winners)[1]
     }
--- RProg/Assignment 3/rankhospital.R
+++ RProg/Assignment 3/rankhospital.R
@@ -1,10 +1,36 @@
 rankhospital <- function(state, outcome, num = "best") {
-    ## Read outcome data
-    
+
+    ## Read (and structure) outcome data
+    columnClasses <- replicate(46, NULL)
+    columnClasses[c(2, 7, 11, 17, 23)] <- "character"
+    outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses=columnClasses,
+                             na.strings="Not Available")
+    colnames(outcomeData) <- c("hospitalName", "stateID", "heartAttack", 
+                               "heartFailure", "pneumonia")
+    outcomeData$heartAttack <- as.numeric(outcomeData$heartAttack)
+    outcomeData$heartFailure <- as.numeric(outcomeData$heartFailure)
+    outcomeData$pneumonia <- as.numeric(outcomeData$pneumonia)
+        
     ## Check that state and outcome are valid
+    validOutcome <- outcome %in% c("heart attack", "heart failure", "pneumonia")
+    validState   <- state %in% outcomeData$stateID
+    if (!validOutcome) { stop("invalid outcome") }
+    if (!validState)   { stop("invalid state") }
+    
+    outcomeData <- outcomeData[outcomeData$stateID == state, ]
+    outcomeData <- switch(outcome, 
+                          "heart attack" = outcomeData[with(outcomeData, order(outcomeData$heartAttack, outcomeData$hospitalName, na.last=NA)), ],
+                          "heart failure" = outcomeData[with(outcomeData, order(outcomeData$heartFailure, outcomeData$hospitalName, na.last=NA)), ],
+                          "pneumonia" = outcomeData[with(outcomeData, order(outcomeData$pneumonia, outcomeData$hospitalName, na.last=NA)), ])
+    outcomeData$rank <- seq_along(outcomeData$hospitalName)
+    
+    if (num == "best") { num <- 1 }
+    if (num == "worst") { num <- length(outcomeData$rank) }
+    # if num is some string other than "best" | "worst" we don't know what to do with it
+    if (class(num) == "character") { stop("invalid rank requested") }
+    if (num > length(outcomeData$rank)) { return(NA) }
     
     ## Return hospital name in that state with the given rank
-    
-    ## 30-day death rate
+    outcomeData[outcomeData$rank == num , "hospitalName"]
     
 }
