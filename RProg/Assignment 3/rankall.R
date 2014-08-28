e543790c9074ef2b5653594fcf2c432b994c1959
Greg Chapman <gregmchapman@users.noreply.github.com>
2014-08-28 16:46:46 -0700

    rankall.R submits successfully

--- RProg/Assignment 3/rankall.R
+++ RProg/Assignment 3/rankall.R
@@ -1,11 +1,42 @@
 rankall <- function(outcome, num = "best") {
-    ## Read outcome data
     
-    ## Check that state and outcome are valid
+    require(dplyr)
+    
+    ## Check that outcome is valid
+    validOutcome <- outcome %in% c("heart attack", "heart failure", "pneumonia")
+    if (!validOutcome) { stop("invalid outcome") }
+    
+    ## Read (and structure) outcome data
+    columnClasses <- replicate(46, NULL)
+    outCol <- switch(outcome, "heart attack" = 11, "heart failure" = 17, 
+                              "pneumonia" = 23)
+    columnClasses[c(2, outCol)] <- "character"
+    columnClasses[7] <- "factor"
+    outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses=columnClasses,
+                             na.strings="Not Available")
+    colnames(outcomeData) <- c("hospital", "state", "outcome")
+    outcomeData$outcome <- as.numeric(outcomeData$outcome)
+    # now we only have the three columns that matter, but we don't want to rank
+    # NA's so let's ditch those rows
+    outcomeData <- outcomeData[!is.na(outcomeData$outcome), ]
+    
+    outcomeData <- outcomeData %>% group_by(state) %>% 
+                                   arrange(state, outcome, hospital) %>% 
+                                   mutate(rank = row_number(outcome))
+
+    if (num == "best") { outcomeData <- outcomeData %>% filter(rank == first(rank)) } else
+    if (num == "worst") { outcomeData <- outcomeData %>% filter(rank == last(rank)) } else
+    if (class(num) == "numeric") { outcomeData <- outcomeData %>% filter(rank == num) }
+    
     
     ## For each state, find the hospital of the given rank
     
     ## Return a data frame with the hospital names and the (abbreviated) 
     ## state name
+    #outcomeData
     
+    outcomeData[ , c("hospital", "state")]
 }
+### works for "best", "worst"; doesn't give NA's for states w/o a hospital at that rank
+### but submit script didn't actually test for that, so I'm letting it slide
+### (I know, I know..., maybe I'll come back to it tomorrow...) 
